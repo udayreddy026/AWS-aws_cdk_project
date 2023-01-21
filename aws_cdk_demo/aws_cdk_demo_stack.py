94da6246cd9866c7be3f410abe27c32948a32966
Uday T <uday.t@idp.com>
2023-01-21 10:13:53 +0530

    Initilizing AWS Demo Project With CDK

--- aws_cdk_demo/aws_cdk_demo_stack.py
+++ aws_cdk_demo/aws_cdk_demo_stack.py
@@ -1,11 +1,9 @@
 from constructs import Construct
+
 from aws_cdk import (
-    Duration,
     Stack,
-    aws_iam as iam,
-    aws_sqs as sqs,
-    aws_sns as sns,
-    aws_sns_subscriptions as subs,
+    aws_lambda as _lambda,
+    aws_apigateway as apigw,
 )
 
 
@@ -14,13 +12,49 @@ class AwsCdkDemoStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        queue = sqs.Queue(
-            self, "AwsCdkDemoQueue",
-            visibility_timeout=Duration.seconds(300),
+        # queue = sqs.Queue(
+        #     self, "Demo-Queue-20221214",
+        #     visibility_timeout=Duration.seconds(300),
+        # )
+
+        # topic = sns.Topic(
+        #     self, "Demo-Topic-20221214"
+        # )
+
+        # topic.add_subscription(subs.SqsSubscription(queue))
+
+        # Creating Lambda Layer
+        boto3_layer = _lambda.LayerVersion(self, "Boto3",
+                                           code=_lambda.Code.from_asset(
+                                               'layers/boto3'),
+                                           compatible_runtimes=[
+                                               _lambda.Runtime.PYTHON_3_8, _lambda.Runtime.PYTHON_3_9],
+                                           compatible_architectures=[
+                                               _lambda.Architecture.X86_64],
+                                           description="Boto3 Common Lambda Layer.",
+                                           )
+
+        aws_xray_layer = _lambda.LayerVersion(self, "aws-x-ray",
+                                              code=_lambda.Code.from_asset(
+                                                  'layers/aws_xray'),
+                                              compatible_runtimes=[
+                                                  _lambda.Runtime.PYTHON_3_8, _lambda.Runtime.PYTHON_3_9],
+                                              compatible_architectures=[
+                                                  _lambda.Architecture.X86_64],
+                                              description="X-Ray Lambda Layer.",
+                                              )
+
+        # Defines an AWS Lambda resource
+        my_lambda = _lambda.Function(
+            self, 'DemoLambda',
+            function_name="Demo_lambda",
+            runtime=_lambda.Runtime.PYTHON_3_9,
+            code=_lambda.Code.from_asset('src'),
+            handler='hello.handler',
+            tracing=_lambda.Tracing.ACTIVE,
+            layers=[boto3_layer, aws_xray_layer],
+            description="Demo Lambda Function."
         )
 
-        topic = sns.Topic(
-            self, "AwsCdkDemoTopic"
-        )
-
-        topic.add_subscription(subs.SqsSubscription(queue))
+        # Creating API Endpoint
+        apigw.LambdaRestApi(self, "/demo", handler=my_lambda)
---
+++ src/hello.py
@@ -0,0 +1,45 @@
+import json
+import boto3
+from typing import Dict
+from aws_xray_sdk.core import xray_recorder
+
+
+def get_secret():
+
+    secret_name = "Mysecrets"
+    region_name = "ap-southeast-2"
+
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    get_secret_value_response = client.get_secret_value(
+        SecretId=secret_name
+    )
+
+    return json.loads(get_secret_value_response['SecretString'])
+
+
+@xray_recorder.capture("Lambda Request")
+def handler(event, context):
+    secrets = get_secret()
+    subsegment = xray_recorder.begin_subsegment('annotations')
+    user_details = {
+        "name": "Abcd",
+        "unique_id": 1234,
+        "address": "xyz country and state.",
+        "zip_code": 123232,
+        "phone_number": 23423423432,
+        "secrets": secrets if secrets is not None else None,
+    }
+
+    subsegment.put_annotation("name", user_details.get("name"))
+    subsegment.put_metadata("body", event)
+    xray_recorder.end_subsegment()
+    return {
+        'statusCode': 200,
+        'headers': {'Content-Type': 'text/plain'},
+        'body': json.dumps(event),
+    }
